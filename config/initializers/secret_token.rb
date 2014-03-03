5bed79b61b87bb79e773cc410ba38dd4901b22cc
Ken Mazaika <kmazaika@gmail.com>
2014-03-03 13:10:28 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CatFacts::Application.config.secret_key_base = '2e3717444a6d404e55f0a3a31e0f8c5fb181600dbc97fff2866a84075f323b92c377abc34ac069ffb56c0e042125248a3d81f95dd923401154e1815510b78e49'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CatFacts::Application.config.session_store :cookie_store, key: '_cat-facts_session'
